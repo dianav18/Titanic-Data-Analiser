2248936478e33d073fb72a4f271aa2f5fb34d391
root <root@Jarvis>
2024-05-31 00:02:19 +0300

    iqr

---
+++ part1.py
@@ -0,0 +1,180 @@
+import pandas as pd
+from io import StringIO
+import matplotlib.pyplot as plt
+import numpy as np
+import re
+import seaborn as sns
+#TASK 1
+
+df = pd.read_csv('train.csv')
+num_columns = df.shape[1]
+print("Number of columns:", num_columns)
+print("\nData type for every coloumn:")
+print(df.dtypes)
+print("\nNumber of absent values for every column:")
+print(df.isnull().sum())
+num_rows = df.shape[0]
+print("\nThe total number of lines:", num_rows)
+duplicates = df.duplicated().any()
+print("\nThere are duplicated lines:", duplicates)
+
+# TASK 2
+
+survived_rate = df['Survived'].mean() * 100
+not_survived_rate = 100 - survived_rate
+class_counts = df['Pclass'].value_counts(normalize=True) * 100
+gender_counts = df['Sex'].value_counts(normalize=True) * 100
+fig, axs = plt.subplots(2, 2, figsize=(14, 10))
+axs[0, 0].bar(['Survived', 'Not Survived'], [survived_rate, not_survived_rate], color=['blue', 'red'])
+axs[0, 0].set_title('Survival Rates')
+axs[0, 0].set_ylabel('Percentage')
+class_counts.plot(kind='bar', ax=axs[0, 1], color='green')
+axs[0, 1].set_title('Passenger Class Distribution')
+axs[0, 1].set_ylabel('Percentage')
+gender_counts.plot(kind='bar', ax=axs[1, 0], color='purple')
+axs[1, 0].set_title('Gender Distribution')
+axs[1, 0].set_ylabel('Percentage')
+plt.tight_layout()
+
+plt.show()
+
+#TASK 3
+
+fig, axs = plt.subplots(2, 3, figsize=(18, 10))
+numeric_columns = ['Age', 'Fare', 'Pclass', 'SibSp', 'Parch']
+for i, col in enumerate(numeric_columns):
+    ax = axs[i//3, i%3]
+    df[col].hist(ax=ax, bins=10, color='skyblue', edgecolor='black')
+    ax.set_title(f'Histogram for {col}')
+    ax.set_xlabel(col)
+    ax.set_ylabel('Frequency')
+axs[1, 2].axis('off')
+
+plt.tight_layout()
+plt.show()
+
+#TASK 4
+
+missing_data = df.isnull().sum()
+missing_columns = missing_data[missing_data > 0]
+missing_proportions = (missing_columns / len(df)) * 100
+print("Coloumns with missing values:")
+print(missing_proportions)
+missing_by_survival = df.groupby('Survived').apply(lambda x: x.isnull().sum() / len(x) * 100)
+print("\nThe procent for every missing value:")
+print(missing_by_survival[missing_columns.index])
+
+#TASK 5
+
+bins = [0, 20, 40, 60, np.inf]
+labels = [0, 1, 2, 3]
+df['AgeCategory'] = pd.cut(df['Age'], bins=bins, labels=labels, right=False)
+age_category_counts = df['AgeCategory'].value_counts().sort_index()
+plt.figure(figsize=(10, 6))
+age_category_counts.plot(kind='bar', color='teal')
+plt.title('Number of Passengers by Age Category')
+plt.xlabel('Age Category')
+plt.ylabel('Number of Passengers')
+plt.xticks(ticks=np.arange(len(labels)), labels=['0-20', '21-40', '41-60', '61+'], rotation=0)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+#TASK 6
+
+male_survival = df[(df['Sex'] == 'male') & (df['Survived'] == 1)]['AgeCategory'].value_counts().sort_index()
+total_males = df[df['Sex'] == 'male']['AgeCategory'].value_counts().sort_index()
+male_survival_rate = (male_survival / total_males) * 100
+plt.figure(figsize=(10, 6))
+male_survival_rate.plot(kind='bar', color='orange')
+plt.title('Survival Rate of Males by Age Category')
+plt.xlabel('Age Category')
+plt.ylabel('Survival Rate (%)')
+plt.xticks(ticks=np.arange(len(labels)), labels=['0-20', '21-40', '41-60', '61+'], rotation=0)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+#TASK 7
+
+total_passengers = len(df)
+num_children = (df['Age'] < 18).sum()
+percent_children = (num_children / total_passengers) * 100
+children_survival_rate = df[df['Age'] < 18]['Survived'].mean() * 100
+adults_survival_rate = df[df['Age'] >= 18]['Survived'].mean() * 100
+plt.figure(figsize=(8, 5))
+plt.bar(['Children', 'Adults'], [children_survival_rate, adults_survival_rate], color=['blue', 'green'])
+plt.title('Survival Rate by Age Group')
+plt.ylabel('Survival Rate (%)')
+plt.ylim(0, 100)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+print(f"Procentul de copii la bord: {percent_children:.2f}%")
+
+#TASK 8
+
+mean_age_survived = df[df['Survived'] == 1]['Age'].mean()
+mean_age_not_survived = df[df['Survived'] == 0]['Age'].mean()
+df.loc[(df['Age'].isnull()) & (df['Survived'] == 1), 'Age'] = mean_age_survived
+df.loc[(df['Age'].isnull()) & (df['Survived'] == 0), 'Age'] = mean_age_not_survived
+most_common_embarked = df['Embarked'].mode()[0]
+df['Embarked'].fillna(most_common_embarked, inplace=True)
+print("Valori lipsă după completare pentru 'Age':", df['Age'].isnull().sum())
+print("Valori lipsă după completare pentru 'Embarked':", df['Embarked'].isnull().sum())
+df.head()
+
+#TASK 9
+df['Title'] = df['Name'].apply(lambda name: re.search(r'\b(\w+)\.', name).group(1) if re.search(r'\b(\w+)\.', name) else 'Unknown')
+def check_title_sex(row):
+    title_sex_mapping = {
+        'Mr': 'male',
+        'Miss': 'female',
+        'Mrs': 'female',
+        'Master': 'male',
+        'Don': 'male',
+        'Dona': 'female',
+        'Lady': 'female',
+        'Sir': 'male',
+        'Countess': 'female',
+        'Dr': 'neutral',
+        'Rev': 'neutral',
+        'Mme': 'female',
+        'Ms': 'female',
+        'Major': 'male',
+        'Capt': 'male',
+        'Jonkheer': 'male'
+    }
+    expected_sex = title_sex_mapping.get(row['Title'], 'neutral')
+    if expected_sex == 'neutral':
+        return True
+    return row['Sex'] == expected_sex
+df['Title_Sex_Correct'] = df.apply(check_title_sex, axis=1)
+correct = df['Title_Sex_Correct'].sum()
+incorrect = len(df) - correct
+print(f"Număr de titluri corecte: {correct}")
+print(f"Număr de titluri incorecte: {incorrect}")
+title_counts = df['Title'].value_counts()
+
+plt.figure(figsize=(10, 6))
+title_counts.plot(kind='bar', color='c')
+plt.title('Distribuția titlurilor de noblete în funcție de numărul de persoane')
+plt.xlabel('Titlu')
+plt.ylabel('Număr de persoane')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+#TASK 10
+
+df['Alone'] = ((df['SibSp'] == 0) & (df['Parch'] == 0)).astype(int)
+plt.figure(figsize=(8, 5))
+sns.histplot(data=df, x='Alone', hue='Survived', multiple='stack', discrete=True, palette='pastel')
+plt.title('Survival Based on Being Alone on Titanic')
+plt.xlabel('Alone (1 = Yes, 0 = No)')
+plt.ylabel('Count')
+plt.xticks([0, 1], ['Not Alone', 'Alone'])
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+sns.catplot(data=df.head(100), x='Pclass', y='Fare', hue='Survived', kind='swarm', height=5, aspect=1.5)
+plt.title('Fare vs. Class vs. Survival for the First 100 Records')
+plt.xlabel('Passenger Class')
+plt.ylabel('Fare')
+plt.grid(True)
+plt.show()
---
+++ part2.py
@@ -0,0 +1,149 @@
+import pandas as pd
+from io import StringIO
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from scipy.stats import zscore
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+def iqr(data):
+    """
+    Elimina outlierii din coloana `Age` folosind metoda Interquartile Range (IQR).
+    :param data: Date de intrare
+    :return: Date curatate fara outlieri in coloana `Age`
+    """
+    ages = data["Age"]
+    q1 = np.percentile(data["Age"], 25)  # Primul quartil (Q1)
+    q3 = np.percentile(data["Age"], 75)  # Al treilea quartil (Q3)
+    iqr_var = q3 - q1  # Intervalul interquartil
+    low = q1 - 1.5 * iqr_var  # Limita inferioara
+    high = q3 + 1.5 * iqr_var  # Limita superioara
+
+    return data[(ages >= low) & (ages <= high)]  # Returneaza datele fara outlieri
+
+def fill_empty(data):
+    """
+    Completeaza valorile lipsa din coloana `Age` cu media varstelor.
+    :param data: Date de intrare
+    :return: Date cu valorile lipsa completate
+    """
+    ages = data["Age"]
+    age_mean = ages.mean()  # Calculeaza media varstelor
+    ages.fillna(age_mean, inplace=True)  # Completeaza valorile lipsa cu media
+    return data
+
+def z_score(data):
+    """
+    Elimina valorile extreme din coloana `Age` folosind scorul z.
+    :param data: Date de intrare
+    :return: Date curatate folosind scorul z
+    """
+    data["Age_z_score"] = zscore(data["Age"])  # Calculeaza scorul z pentru fiecare valoare din `Age`
+    max_z_value = 3  # Valoarea maxima a scorului z acceptata
+    return data[(data["Age_z_score"] > -max_z_value) & (data["Age_z_score"] < max_z_value)]  # Returneaza datele fara valorile extreme
+
+def verify(data):
+    """
+    Verifica tipul de date al coloanei `Age` si afiseaza o histograma a distributiei varstelor.
+    :param data: Date de intrare
+    """
+    if data["Age"].dtype != "int64" and data["Age"].dtype != "float64":
+        return  # Verifica daca tipul de date al coloanei `Age` este numeric
+
+    plt.title("Age")
+    data["Age"].hist()  # Afiseaza o histograma a distributiei varstelor
+    plt.show()
+
+def train(training_data):
+    """
+    Antreneaza un model Random Forest Classifier folosind datele de antrenament curatate.
+    :param training_data: Date de antrenament curatate
+    :return: Modelul antrenat Random Forest Classifier
+    """
+    model = RandomForestClassifier()
+    training_data = training_data.copy()
+    training_data = training_data.drop(["Name", "Ticket", "Cabin", "Age_z_score"], axis=1)  # Elimina coloanele irelevante
+
+    gender_map = {"male": 1, "female": 2}  # Mapare pentru coloana `Sex`
+    embarked_map = {"C": 1, "Q": 2, "S": 3}  # Mapare pentru coloana `Embarked`
+
+    training_data["Sex"] = training_data["Sex"].replace(gender_map)  # Transforma valorile categorice in numerice
+    training_data["Embarked"] = training_data["Embarked"].replace(embarked_map)
+
+    X = training_data.drop("Survived", axis=1)  # Caracteristicile de antrenament
+    y = training_data["Survived"]  # Etichetele de antrenament
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)  # imparte datele in seturi de antrenament si testare
+
+    model.fit(X_train, y_train)  # Antreneaza modelul
+    training_predictions = model.predict(X_test)  # Realizeaza predictii pe setul de testare
+
+    accuracy = accuracy_score(y_test, training_predictions)
+    print(f"Accuracy: {accuracy * 100:.2f}%")  # Afiseaza acuratetea modelului
+
+    importance = model.feature_importances_  # Importanta caracteristicilor
+    indices = np.argsort(importance)[::-1]
+    names = [X.columns[i] for i in indices]
+
+    plt.figure()
+    plt.title(f"RandomForestClassifier Feature Importance")
+    plt.bar(range(X.shape[1]), importance[indices])
+    plt.xticks(range(X.shape[1]), names, rotation=90)  # Afiseaza importanta caracteristicilor
+    plt.savefig(f"RandomForestClassifier_feature_importance.png")
+    plt.show()
+
+    return model  # Returneaza modelul antrenat
+
+def predict(__real_data, model):
+    """
+    Foloseste modelul antrenat pentru a prezice supravietuirea pasagerilor din setul de date de test.
+    :param __real_data: Date reale de test
+    :param model: Modelul antrenat Random Forest Classifier
+    """
+    real_data: pd.DataFrame = __real_data.copy()
+
+    gender_map = {"male": 1, "female": 2}  # Mapare pentru coloana `Sex`
+    embarked_map = {"C": 1, "Q": 2, "S": 3}  # Mapare pentru coloana `Embarked`
+
+    real_data = real_data.drop(["Name", "Ticket", "Cabin"], axis=1)  # Elimina coloanele irelevante
+    real_data["Sex"] = real_data["Sex"].replace(gender_map)  # Transforma valorile categorice in numerice
+    real_data["Embarked"] = real_data["Embarked"].replace(embarked_map)
+
+    real_x = real_data.drop("Survived", axis=1)  # Caracteristicile de test
+    real_y = real_data["Survived"]  # Etichetele de test
+
+    real_predictions = model.predict(real_x)  # Realizeaza predictiile
+
+    accuracy = accuracy_score(real_y, real_predictions)
+    print(f"Accuracy: {accuracy * 100:.2f}%")  # Afiseaza acuratetea predictiilor
+
+    real_data["Survived"] = real_predictions  # Adauga predictiile in datele reale
+    real_data.to_csv(f"RandomForestClassifier_predictions.csv")  # Salveaza predictiile intr-un fisier CSV
+
+def main():
+    data = pd.read_csv("train.csv")
+    data.set_index("PassengerId", inplace=True)
+
+    verify(data)  # Verifica distributia initiala a varstelor
+
+    data = fill_empty(data)  # Completeaza valorile lipsa
+    data = iqr(data)  # Elimina outlierii
+    data = z_score(data)  # Elimina valorile extreme
+
+    verify(data)  # Verifica distributia varstelor dupa curatare
+
+    model = train(data)  # Antreneaza modelul
+    test_data = pd.read_csv("test.csv")
+    correct_predictions = pd.read_csv("gender_submission.csv")
+
+    test_data.set_index("PassengerId", inplace=True)
+    correct_predictions.set_index("PassengerId", inplace=True)
+
+    test_data = pd.merge(test_data, correct_predictions, on="PassengerId")  # Fuzioneaza datele de test cu predictiile corecte
+
+    predict(test_data, model)  # Realizeaza predictiile folosind modelul antrenat
+
+if __name__ == "__main__":
+    main()  # Ruleaza functia principala
